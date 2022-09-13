c2e5032abd3969cf90a0459c4f7465013b7d60bf
Liandro <mateuzinho1@live.com>
2022-09-13 12:01:07 -0300

    Seleção de estados

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,39 @@
 // Function
-const aviso = (msg)=>{
-    alert(msg)
+const aviso = (msg) => {
+  alert(msg)
 }
 
-const mostraIdade = ()=>{
-    let span = document.getElementById('txt-idade')
-    let campoIdade = document.getElementById('idade')
-    span.innerText = campoIdade.value
+const mostraIdade = () => {
+  let span = document.getElementById('txt-idade')
+  let campoIdade = document.getElementById('idade')
+  span.innerText = campoIdade.value
 }
 
+var dataAtual = new Date()
+
+const mostraData = () => {
+  let dia = dataAtual.getDay()
+  let mes = dataAtual.getMonth() + 1
+  let ano = dataAtual.getFullYear()
+  let hora = dataAtual.getHours()
+  let valor = dia + '/' + mes + '/' + ano + '-' + hora
+
+  document.getElementById('dt-cadastro').value = valor
+}
+
+//Preenche o select "estado" com os estados da API do IBGE
+const getEstados = () => {
+  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+  let select = document.getElementById('estado')
+
+  // Lê a apai através do fecth(),1º then captura os dados, 2º then trata os dados
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    let options = '<option>Selecione</option>'
+
+    select.innerHTML = options
+  })
+
+}
 
 
 
@@ -24,25 +49,31 @@ const mostraIdade = ()=>{
 //     aviso(ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh);
 // }
 // )
+getEstados()
 
 mostraIdade()
-document.getElementById('idade').addEventListener('change',mostraIdade)
+document.getElementById('idade').addEventListener('change', mostraIdade)
+
+mostraData()
 
 
 // inicializa animações scroll do AOS 
 AOS.init();
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// Impede o envio do formulário quando os campos estão invalidos 
+
 // função seta =>
+
 (() => {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // Variavel captura as tags <form> que contem a classe 'needs-validation'
   const forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
+  // Executa para cada formulário da variavel form
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
+      // se houver campos invalidos, interrompe o submit
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
